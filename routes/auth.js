afeefdf80c369c7f41e0fc0438897802ac9edff3
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-02-22 10:19:14 -0800

    working on auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,44 @@
 "use strict";
 
+const User = require("../models/user");
+
+const { UnauthorizedError } = require("../expressError");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
 const Router = require("express").Router;
 const router = new Router();
 
 /** POST /login: {username, password} => {token} */
+
+router.post("/login", async function () {
+
+    const { username, password } = req.body;
+
+
+    if (User.authenticate(username, password)) {
+        const token = jwt.sign({ username }, SECRET_KEY);
+        console.log("HERE IS THE TOKEN",token);
+        return res.json({token});
+    };
+
+
+    throw new UnauthorizedError;
+
+});
+
+
 
 
 /** POST /register: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post("/register", async function () {
+
+    const { username, password, first_name, last_name, phone } = req.body;
+
+})
+
 
 module.exports = router;
